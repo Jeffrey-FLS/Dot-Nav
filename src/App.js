ebca39afa39d6298ffb3b347d6e2428edc7fcd6c
Jeff <JP7Kode@gmail.com>
2020-03-18 13:23:19 -0400

    Done, pushing to Git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  const numOfSections = 6;
+
+  const renderSections = num => {
+    let arr = new Array(num);
+
+    for (let i = 1; i < num; i++) {
+      arr[i] = `section-${i}`;
+    }
+
+    return arr;
+  };
+
+  const randColorGenerator = () => {
+   return "#" + Math.floor(Math.random()*16777215).toString(16);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        {renderSections(numOfSections).map(ele => (
+          <div id={ele} style={{backgroundColor: randColorGenerator()}}>
+            <h1>{ele}</h1>
+          </div>
+        ))}
+        <div className="dot-nav">
+          <ul>
+            {renderSections(numOfSections).map(ele => (
+              <li>
+                <h2><a href={`#${ele}`}>{ele}</a></h2>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
